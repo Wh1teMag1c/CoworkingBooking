a10cbc6a305d1a5d226043ba0853707bd8f002ba
Dmitry Bartashevskiy <dmitrybwm@yandex.ru>
2026-05-16 00:18:37 +0300

    create views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .models import User, Category, Amenity, Room, Booking, Review
+from .serializers import (
+    UserSerializer, CategorySerializer, AmenitySerializer,
+    RoomSerializer, BookingSerializer, ReviewSerializer
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class AmenityViewSet(viewsets.ModelViewSet):
+    queryset = Amenity.objects.all()
+    serializer_class = AmenitySerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.filter(is_active=True)
+    serializer_class = RoomSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff or (not user.is_anonymous and user.role == 'manager'):
+            return Booking.objects.all()
+        if not user.is_anonymous:
+            return Booking.objects.filter(user=user)
+        return Booking.objects.none()
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
