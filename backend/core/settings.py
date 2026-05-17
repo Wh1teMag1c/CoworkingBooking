35b2a951f290eaefba25d9e54b2156d30d96f8da
Dmitry Bartashevskiy <dmitrybwm@yandex.ru>
2026-05-17 21:43:02 +0300

    add docker-compose, dockerfiles, and nginx proxy setup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m833jjzwfdy-n=bi&^n^_+e9gfsteipe@!(y^)(3xhvbomhqv!'
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -136,6 +138,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
